a9a17220236dc8db881cd80f9c74d6486bb5cdca
M1TKO <mitko.kalenderov@gmail.com>
2017-07-03 13:54:00 +0300

    Fixed wrong sql queries for import in the MySQL dbms

--- my_note.sql
+++ my_note.sql
@@ -1,26 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2017 at 11:45 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `my_note`
 --
+
 -- --------------------------------------------------------
---it is optimal (you can name the db how you want)
--- CREATE DATABASE `my_note`;
--- USE `my_note`;
-
 
 --
---  Table structure for table `notes`
+-- Creating the database `my_note`
 --
 
+CREATE DATABASE `my_note`;
 
-CREATE TABLE `users` (
-  `user_idAULT CURRENT_TIMESTAMP
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(40) NOT NULL,
+  `body` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 --
--- Indexes for table `users`
+-- Table structure for table `users`
 --
 
+CREATE TABLE `users` (
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `username` (`username`);
-  
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
